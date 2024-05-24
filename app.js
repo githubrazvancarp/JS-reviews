7dc6bbe9bfdd1151378f8121863d2b92091d5bbc
Razvan Carp <razvan_ector@yahoo.com>
2024-05-24 13:47:46 +0300

    for commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const reviews = [
+{
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img: "",
+    text: "",    
+},
+
+{
+    id: 2,
+    name: "anna johnson",
+    job: "web developer",
+    img: "",
+    text: "",    
+},
+
+{
+    id: 3,
+    name: "peter jones johnson",
+    job: "intern",
+    img: "",
+    text: "",    
+},
+
+{
+    id: 4,
+    name: "bill anderson",
+    job: "boss",
+    img: "",
+    text: "",    
+},
+
+];
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(|"prev-btn");
+const nextBtn = document.querySelector(|"next-btn");
+const randomBtn = document.querySelector(|"random-btn");
