e96717757dc101c028839a97feb68901d7ac8a56
Razvan Carp <razvan_ector@yahoo.com>
2024-07-19 16:36:15 +0300

    final touch

--- app.js
+++ app.js
@@ -3,41 +3,86 @@ const reviews = [
     id: 1,
     name: "susan smith",
     job: "web developer",
-    img: "",
-    text: "",    
+    img: "./images/Avatar.2.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, dolorem soluta impedit voluptas et vero.",    
 },
-
 {
     id: 2,
     name: "anna johnson",
     job: "web developer",
-    img: "",
-    text: "",    
+    img: "./images/Avatar.1.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, dolorem soluta impedit voluptas et vero.",    
 },
-
 {
     id: 3,
     name: "peter jones johnson",
     job: "intern",
-    img: "",
-    text: "",    
+    img: "./images/Avatar.2.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, dolorem soluta impedit voluptas et vero.",    
 },
-
 {
     id: 4,
     name: "bill anderson",
     job: "boss",
-    img: "",
-    text: "",    
+    img: "./images/Avatar.3.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, dolorem soluta impedit voluptas et vero.",    
 },
-
 ];
 
-const img = document.getElementById("person-img");
+// select items
+const img = document.getElementById("person-img");                               
 const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
 
-const prevBtn = document.querySelector(|"prev-btn");
-const nextBtn = document.querySelector(|"next-btn");
-const randomBtn = document.querySelector(|"random-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+    showPerson();
+});
+
+// show person based on item
+
+function showPerson(){
+    const item = reviews[currentItem]
+    img.src = item.img;
+    author.textContent = item.name;
+    job.textContent = item.job;
+    info.textContent = item.text;
+}
+
+// show next person
+
+nextBtn.addEventListener('click', function () {
+    currentItem++;
+    showPerson(currentItem);
+    if (currentItem > reviews.length - 1) {
+        currentItem = 0;
+    }
+    showPerson();
+});
+
+// show prev person
+
+prevBtn.addEventListener('click', function () {
+    currentItem--;
+    showPerson(currentItem);
+    if (currentItem < 0) {
+        currentItem = reviews.length - 1;
+    }
+    showPerson();
+});
+
+// show random person
+
+randomBtn.addEventListener('click', function () {
+    currentItem = Math.floor(Math.random() * reviews.length)
+    console.log showPerson(): void
+    showPerson();
+});
